e0c4a2e3c8fb2f283b454a62a58936d7dd531906
aayushi401 <aayushi3444@gmail.com>
2022-12-30 12:31:21 +0530

    test

---
+++ contracts/dynamic_array.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.0;
+
+contract dynamicarray{
+    uint[] public arr = [0,10,20,30,40,50];
+    
+    function fun() public{
+        uint temp = arr[3];
+
+        //update
+        arr[0] = 100;
+
+        //delete
+        delete arr[1];
+
+        //push into array
+        arr.push(60);
+
+    }
+
+    function len() public view returns(uint){
+        uint len = arr.length;
+        return len;
+    }
+}
