c33d4b1672a4cf13ace8729c702bc083778f8a60
aayushi401 <aayushi3444@gmail.com>
2022-12-30 11:35:00 +0530

    test

---
+++ contracts/datatypes.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract types{
+    bool public boolvalue = true;
+    uint public uintvalue = 122;
+    int public intvalue = 156;
+    address public adddressvalue = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes16 public b4; 
+}
